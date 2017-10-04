f38311b50fc462d1f17d9131bf13fed484c2ecb7
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 03:12:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,15 @@
 INSERT INTO users (name, age) VALUES ("maki", 20), ("saki", 19), ("yui", 20), ("mai", 22), ("yumi", 18), ("lala", 23), ("mari", 23),
 ("yoko", 25), ("koko", 29), ("yuko", 27), ("mimi", 28), ("mochi", 26), ("mami", 19), ("aki", 21), ("yuki", 20), ("ai", 19), ("airi", 27), ("haru", 22), ("mayu", 28), ("miku", 17);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("help me", "dreams", 100000, "10-2017", "10-2018"), 
+("i'm poor", "bills", 2000, "09-2017", "10-2019"), 
+("i lost my job", "housing", 30000, "08-2017", "12-2017"),
+("self-victimized", "victim", 5000, "11-2017", "12-2019"), 
+("i think i can dance", "dreams", 20000, "10-2017", "12-2020"), 
+("trying not to do drugs", "literature", 1000, "07-2017", "07-2020"),
+("spacial snowflake", "bills", 500000, "05-2017", "01-2019"),
+("i'm going to mordor", "studies", 7000, "08-2017", "01-2018"),
+("i'm an actor...", "bills", 40000, "09-2017", "12-2017"),
+("bae took my wallet", "victim", 10000, "10-2017", "02-2018");
+
