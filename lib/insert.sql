5dd95ddabae36f2b84951390aa2a865bdcbfa18f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 02:59:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (name, age) VALUES ("maki", 20), ("saki", 19), ("yui", 20), ("mai", 22), ("yumi", 18), ("lala", 23), ("mari", 23), 
+INSERT INTO users (name, age) VALUES ("maki", 20), ("saki", 19), ("yui", 20), ("mai", 22), ("yumi", 18), ("lala", 23), ("mari", 23),
 ("yoko", 25), ("koko", 29), ("yuko", 27), ("mimi", 28), ("mochi", 26), ("mami", 19), ("aki", 21), ("yuki", 20), ("ai", 19), ("airi", 27), ("haru", 22), ("mayu", 28), ("miku", 17);
