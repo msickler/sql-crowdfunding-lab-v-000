265855dec6c9c6308f0fb7da66aad0792de1adb1
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 03:50:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, SUM(pledges.amount) FROM users
 INNER JOIN pledges
 ON users.id = user_id
-GROUP BY users.name ORDER BY SUM(pledges.amount) DESC;"
+GROUP BY users.name ORDER BY SUM(pledges.amount) ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
